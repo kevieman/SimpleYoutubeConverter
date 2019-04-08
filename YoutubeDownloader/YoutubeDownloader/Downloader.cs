d9252a1d0209414b7ff086603ff42a1cb85078d7
Kevin Tabak <kevin_tabak@outlook.com>
2019-04-08 21:40:17 +0200

    Initial project

---
+++ YoutubeDownloader/YoutubeDownloader/Downloader.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Windows;
+using MediaToolkit;
+using MediaToolkit.Model;
+using VideoLibrary;
+using YoutubeDownloader.Properties;
+
+namespace YoutubeDownloader
+{
+    class Downloader
+    {
+        private string _downloadFolder;
+        private readonly string _appDataFolder;
+        private readonly YouTube _youtube;
+        private string _videosFolder;
+
+        public Downloader(string appDataFolder)
+        {
+            _appDataFolder = appDataFolder;
+            SetSource();
+            CreateVideosFolder();
+
+            _youtube = YouTube.Default;
+        }
+
+        public void SetSource()
+        {
+            _downloadFolder = @Settings.Default.DownloadFolder;
+        }
+
+        public void Download(string url)
+        {
+            YouTubeVideo video;
+
+            if (_downloadFolder.Equals("none"))
+            {
+                MessageBox.Show("Set a download folder!");
+                return;
+            }
+
+            try
+            {
+                video = _youtube.GetVideo(url);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid URL");
+                return;
+            }
+
+            if (video == null)
+                return;
+            
+            File.WriteAllBytes(Path.Combine(_videosFolder, video.FullName), video.GetBytes());
+
+            var inputFile = new MediaFile { Filename = Path.Combine(_videosFolder, video.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(_downloadFolder, $"{video.FullName}.mp3") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
+
+            File.Delete(Path.Combine(_videosFolder, video.FullName));
+            MessageBox.Show("Done!");
+        }
+
+        private void CreateVideosFolder()
+        {
+            _videosFolder = Path.Combine(_appDataFolder, @"Videos");
+
+            if (!Directory.Exists(_videosFolder))
+                Directory.CreateDirectory(_videosFolder);
+        }
+    }
+}
---
+++ YoutubeDownloader/YoutubeDownloader/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using YoutubeDownloader.Properties;
+
+namespace YoutubeDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string _downloadFolder;
+        private string _appDataFolder;
+        private readonly Downloader _downloader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateLocalAppFolder();
+
+            _downloader = new Downloader(_appDataFolder);
+
+            SetDownloadFolder();
+        }
+
+        private void BrowseFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if(result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    Debug.WriteLine(fbd.SelectedPath);
+                    Settings.Default.DownloadFolder = fbd.SelectedPath;
+                    Settings.Default.Save();
+                    SetDownloadFolder();
+                }
+            }
+            Debug.WriteLine($"Setting: {(string)Settings.Default["DownloadFolder"]}");
+        }
+
+        private void SetDownloadFolder()
+        {
+            Settings.Default.DownloadFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            _downloadFolder = Settings.Default.DownloadFolder;
+            _downloader.SetSource();
+            FolderLocationTextBlock.Text = _downloadFolder;
+        }
+
+        private void Reset()
+        {
+            Debug.WriteLine("Reset");
+            Settings.Default.Reset();
+            SetDownloadFolder();
+        }
+
+        private void OpenFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", _downloadFolder);
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            LinkTextBox.IsEnabled = false;
+            DownloadButton.IsEnabled = false;
+            _downloader.Download(LinkTextBox.Text);
+            LinkTextBox.Text = "";
+            LinkTextBox.IsEnabled = true;
+            DownloadButton.IsEnabled = true;
+        }
+
+        private void CreateLocalAppFolder()
+        {
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _appDataFolder = Path.Combine(localAppData, @"KProducts\YoutubeDownloader");
+
+            if (!Directory.Exists(_appDataFolder))
+                Directory.CreateDirectory(_appDataFolder);
+        }
+    }
+}
